372c3a09b81f204cf7061bf687dab657cdb43c65
Russell Alecio Sanchez Ake <114969832+Russy-Okumura@users.noreply.github.com>
2024-12-06 08:18:41 -0500

    Update index.php

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
     <main>
         <!-- Hero Section -->
     <div class="hero">
-        <img src="Imagenes/hero.png" alt="Personas colaborando en actividades comunitarias">
+        <img src="Imagenes/hero.webp" alt="Personas colaborando en actividades comunitarias">
         <h1>Inclunet Space</h1>
         <h2>Conecta con tu propósito: <br> Haz la diferencia hoy</h2>
         <p>Únete a proyectos que transforman comunidades <br> y deja tu huella en el mundo.</p>
