241c28f1743e5890880b4805234e104613d464f5
Russell Alecio Sanchez Ake <114969832+Russy-Okumura@users.noreply.github.com>
2024-12-09 23:36:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -45,9 +45,11 @@
                          <!-- Menú desplegable -->
                         <div id="user-menu" class="dropdown-menu">
                             <a href="profile.html">Mi perfil</a>
-                            <a href="settings.html">Configuración</a>
+                            <?php if (isset($_COOKIE['tipo_usuario']) && $_COOKIE['tipo_usuario'] === 'organizacion') { ?>
+                                <a href="settings.html">Configuración</a>
+                            <?php } ?>
+                            <a href="settings.html">Panel</a>
                             <a href="logout.php" onclick="window.location.reload();">Cerrar sesión</a>
-
                         </div>
                     </div>
                 <?php } ?>
