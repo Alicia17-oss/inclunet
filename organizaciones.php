d2700f0aca52e66a057cedc04d01304c4579d8b2
Russell Alecio Sanchez Ake <114969832+Russy-Okumura@users.noreply.github.com>
2024-12-13 12:34:08 -0500

    Update organizaciones.php

--- organizaciones.php
+++ organizaciones.php
@@ -8,7 +8,7 @@
     <meta name="keywords" content="Organizaciones, inclusión, cambio social, aliados">
     <meta name="author" content="Inclunet Space">
     <link rel="stylesheet" href="stylesG.css">
-    <link rel="stylesheet" href="stylesG.css">
+    <link rel="stylesheet" href="stylesO.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer">
     <script src="script.js" defer></script>
 </head>
